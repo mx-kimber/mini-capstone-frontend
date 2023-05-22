9bd98daf8e861f08b348d185285f1143d057e2c1
Kimber Ogden <mx.kimber@gmail.com>
2023-05-21 20:57:49 -0500

    Product Show, added scroll to modal, cleaned up indentation in Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -22,40 +22,58 @@ export function Content() {
      });
    };
 
-    const handleCreateProduct = (params, successCallback) => {
-      console.log("handleCreateProduct", params);
-      axios.post("http://localhost:3000/products.json", params).then((response) => {
-        setProducts([...products, response.data]);
-        successCallback();
-      });
-    };
+  const handleCreateProduct = (params, successCallback) => {
+    console.log("handleCreateProduct", params);
+    axios.post("http://localhost:3000/products.json", params).then((response) => {
+      setProducts([...products, response.data]);
+      successCallback();
+    });
+  };
 
-    const handleShowProduct = (product) => {
-        console.log("handleShowProduct", product);
-        setIsProductsShowVisible(true);
-        setCurrentProduct(product);
-      };
-      
-      const handleClose = () => {
-        console.log("handleClose");
-        setIsProductsShowVisible(false);
-      };
+  const handleShowProduct = (product) => {
+    console.log("handleShowProduct", product);
+    setIsProductsShowVisible(true);
+    setCurrentProduct(product);
+  };
 
-   useEffect(handleIndexProducts, []);
-   
-    return (
-      <div>
-      <Signup />
-      <Login />
-      <LogoutLink />
-      <ProductsNew onCreateProduct={handleCreateProduct} />
-      <ProductsIndex products={products} onShowProduct={handleShowProduct} />
+  const handleUpdateProduct = (id, params, successCallback) => {
+    console.log("handleUpdateProduct ", params);
+    axios.patch(`http://localhost:3000/products/${id}.json`, params).then((response) => {
+      setProducts(
+        products.map((product) => {
+          if (product.id === response.data.id) {
+            return response.data;
+          } else {
+            return product;
+          }
+        })
+      );
+      successCallback();
+      handleClose();
+    });
+  };
+  
+    
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsProductsShowVisible(false);
+  };
 
-      <Modal show={isProductsShowVisible} onClose={handleClose}>
-          <h1>Modal Test</h1>
-          <ProductsShow product={currentProduct} />
-        </Modal>
+  useEffect(handleIndexProducts, []);
+  
+  return (
+    <div>
+    <Signup />
+    <Login />
+    <LogoutLink />
+    <ProductsNew onCreateProduct={handleCreateProduct} />
+    <ProductsIndex products={products} onShowProduct={handleShowProduct} />
 
-      </div>
-    );
-  }
+    <Modal show={isProductsShowVisible} onClose={handleClose}>
+        <h1>Modal Test</h1>
+        <ProductsShow product={currentProduct} onUpdateProduct={handleUpdateProduct} />
+      </Modal>
+
+    </div>
+  );
+}
--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,12 +1,39 @@
 export function ProductsShow(props) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdateProduct(props.product.id, params, () => event.target.reset());
+  };
+
   return (
     <div>
       <h1>Product information</h1>
+      <p>Id: {props.product.id}</p>
       <p>Name: {props.product.name}</p>
-      <p>Supplier: {props.product.supplier.name}</p>
+      <p>Supplier Id: {props.product.supplier.name}</p>
       <p>Price: {props.product.price}</p>
       <p>Quantity: {props.product.quantity}</p>
       <p>Description: {props.product.description}</p>
+      
+      <form onSubmit={handleSubmit}>
+      <div>
+        Name: <input name="name" type="text" defaultValue={props.product.name} />
+      </div>
+      <div>
+        Supplier_id: <input name="supplier_id" type="text"  defaultValue={props.product.supplier.id} />
+      </div>
+      <div>
+        Price: <input name="price" type="integer" defaultValue={props.product.price} />
+      </div>
+      <div>
+        Quantity: <input name="quantity" type="integer" defaultValue={props.product.quantity} />
+      </div>
+      <div>
+        Description: <input name="description" type="text" defaultValue={props.product.description}/>
+      </div>
+        <button type= "submit"> Update Product</button>
+      </form>
     </div>
   );
 }
