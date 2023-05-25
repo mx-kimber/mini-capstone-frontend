288b7442f30fb5f268291238419ffe0f4a1ea5b2
Kimber Ogden <mx.kimber@gmail.com>
2023-05-25 12:29:03 -0500

    added authentican links

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,25 @@ import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
+  let authenticationLinks;
+  if (localStorage.jwt === undefined) {
+    authenticationLinks = <>
+    <a href="/login">Login</a>| <a href="/signup">Signup</a>
+    </>
+  } else {
+    authenticationLinks = <LogoutLink />
+  }
+  
   return (
     <header>
       <nav>
         <a href="/">Home</a> | 
         <Link to="/about">About</Link> | 
         <Link to="/products/new">New Product</Link> |  
-        <Link to="/signup">Signup</Link> | 
-        <Link to="/login">Login</Link> | 
-        <LogoutLink />
+        {authenticationLinks}
+        {/* <Link to="/signup">Signup</Link> | 
+        <Link to="/login">Login</Link> |  */}
+        {/* <LogoutLink /> */}
       </nav>
     </header>
   )
